1a2a31602d36848fcaa65778a62e637a1d9de60d
shamu2811 <tenner.nick01@gmail.com>
2018-04-03 18:47:53 +0200

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -15,20 +15,20 @@
 # limitations under the License.
 #
 
-# Inherit CM common GSM/Phone stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit LineageOS common GSM/Phone stuff.
+$(call inherit-product, vendor/lineage/config/gsm.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/skomer/skomer.mk)
 
 # Device identifier
 PRODUCT_DEVICE := skomer
-PRODUCT_NAME := cm_skomer
+PRODUCT_NAME := lineage_skomer
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-S7710
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint and ID
 BUILD_ID := TRIANA00$(shell date -u +%Y%m%d)
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cm_skomer BUILD_FINGERPRINT=cyanogenmod/cm_skomer/skomer:4.4/$(BUILD_ID) PRIVATE_BUILD_DESC="GT-S7710 4.4"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lineage_skomer BUILD_FINGERPRINT=lineageos/lineage_skomer/skomer:7.1.2/$(BUILD_ID) PRIVATE_BUILD_DESC="GT-S7710 7.1.2"
